65c671b4b6379a7064bf3dcf265e5aa740a00984
Kapil Chopra <kapilchopra963@gmail.com>
2018-06-17 14:39:48 -0400

    Added routes

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api', function(req, res){
-  //GET request made
-  res.send({name:'KC'});
-  res.end();
-});
+app.use('/api', require('./routes/api'));
 
 app.listen(3000, function(){
   console.log('Now Listening to port 3000...');
